398c71e25a02959ac78d968d3d8cce52c6efc5f2
khalideyez <onecampus2019@gmail.com>
2021-10-28 22:47:24 +0300

    footer touched

--- frontend/views/includes/footer.php
+++ frontend/views/includes/footer.php
@@ -2,6 +2,6 @@
 Copyright &copy; 2020 - <?=date('Y')?> &nbsp UDOM-CIVE.
 &nbsp&nbsp&nbsp&nbsp All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-    <strong><a href="#">E-classroom</a></strong><b>Version</b> 2.0
+    <strong><a href="#">E-classroom</a>&nbsp&nbsp</strong><b>Version</b> 2.0
     </div>
   </footer>
