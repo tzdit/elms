4ce3303b0b47d9aa993d1593df25fc92406c6b9d
khalideyez <onecampus2019@gmail.com>
2021-11-03 11:33:08 +0300

    A lot

--- frontend/views/instructor/classExtAssessments.php
+++ frontend/views/instructor/classExtAssessments.php
@@ -26,10 +26,10 @@ use yii\bootstrap4\ActiveForm;
 use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
-$this->params['courseTitle'] = "Course ".$cid;
-$this->title = 'Classwork';
+$this->params['courseTitle'] =$cid." External Assessments";
+$this->title =$cid." External Assessments";
 $this->params['breadcrumbs'] = [
-  ['label'=>'classwork', 'url'=>Url::to(['/instructor/classwork', 'cid'=>$cid])],
+  ['label'=>'class dashboard', 'url'=>Url::to(['/instructor/class-dashboard', 'cid'=>$cid])],
   ['label'=>$this->title]
 ];
 
@@ -134,12 +134,7 @@ $assignstudentsmodel = new StudentAssign();
 <?= $this->render('assignstudents', ['assignstudentsmodel'=>$assignstudentsmodel, 'ccode'=>$cid]) ?>
 
 <!--  ###################################render model to create_material ####################################################-->
-<?php 
-$assmodel = new UploadMaterial();
-?>
-<?= $this->render('materials/create_material', ['assmodel'=>$assmodel, 'ccode'=>$cid]) ?>
-<!--  ###################################new assessment modal ####################################################-->
-<?php 
+<?php
 $assessmodel = new External_assess();
 ?>
 <?= $this->render('assessupload', ['assessmodel'=>$assessmodel, 'ccode'=>$cid]) ?>
