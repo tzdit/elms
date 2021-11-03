d395d6ce807ed2d3f536802df13e50d3ff42bc26
OnaNixon <65618257+OnaNixon@users.noreply.github.com>
2021-11-03 00:39:09 -0700

    sss2

---
+++ frontend/views/studentmanage/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Student */
+
+$this->title = 'Update Student: ' . $model->reg_no;
+$this->params['breadcrumbs'][] = ['label' => 'Students', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->reg_no, 'url' => ['view', 'id' => $model->reg_no]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="student-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
