60ab7c4133027a3247ffce662d941f428e7baf64
khalideyez <onecampus2019@gmail.com>
2021-11-03 11:41:43 +0300

    deleting proj

--- frontend/views/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-Copyright &copy; 2020 - <?=date('Y')?> &nbsp The University of Dodoma.
-&nbsp&nbsp&nbsp&nbsp All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-    <strong>UDOM CLASSROOM</strong>&nbsp&nbsp<b>Version</b> 2.0
-    </div>
-  </footer>
