9913bc582ee446af9e36aa5219c8b6f0bd3f02d0
khalideyez <onecampus2019@gmail.com>
2021-11-16 13:51:31 +0300

    material bug fixed

--- frontend/controllers/InstructorController.php
+++ frontend/controllers/InstructorController.php
@@ -1275,7 +1275,7 @@ public function actionUploadMaterial(){
         if($model->upload()){
             
             $secretKey=Yii::$app->params['app.dataEncryptionKey'];
-            $cid=Yii::$app->getSecurity()->encryptByPassword($cid, yii::$app->session->get('ccode'));
+            $cid=Yii::$app->getSecurity()->encryptByPassword(yii::$app->session->get('ccode'),$secretKey);
             Yii::$app->session->setFlash('success', 'Material uploaded successfully');
             return $this->redirect(['class-materials','cid'=>$cid]);
         }else{
