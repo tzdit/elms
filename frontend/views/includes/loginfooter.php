77711ca6285dee87638f0a4af373dbb184cd4791
khalideyez <onecampus2019@gmail.com>
2021-12-13 11:09:00 +0300

    student registration closed

---
+++ frontend/views/includes/loginfooter.php
@@ -0,0 +1,7 @@
+<footer style="position:fixed;bottom:0;color:grey;font-outline:#bbf">
+Copyright &copy; 2020 - <?=date('Y')?> &nbsp The University of Dodoma.
+&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp
+    
+
+    
+  </footer>
