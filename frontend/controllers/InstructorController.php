7262f4d8c26e2077db900976a171e18ff013b453
khalideyez <onecampus2019@gmail.com>
2021-12-28 19:18:21 +0300

    assignment update fix

--- frontend/controllers/InstructorController.php
+++ frontend/controllers/InstructorController.php
@@ -1405,7 +1405,7 @@ public function actionDownloadSubmits($assignment)
 
    try
    {
-   $dir=sys_get_temp_dir();
+   $dir="@runtime/tmp/";
    $ziptmp=$dir."submits_tmp.zip";
    
    $ziptmp=str_replace(' ', '', $ziptmp);
