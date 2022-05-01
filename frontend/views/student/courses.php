aa79b76e4b4fd05171e2d2847468649670bbe43f
josure-pro <57487928+josure-pro@users.noreply.github.com>
2022-05-01 12:40:14 +0300

    display all courses

--- frontend/views/student/courses.php
+++ frontend/views/student/courses.php
@@ -45,8 +45,8 @@ $this->params['breadcrumbs'] = [
 
  
              <div class="row">
-             <div class="col-md-12">
-              <h4><p style="text-align: center;"> <span class="badge bg-primary">First Year Courses</span></p></h4>
+             <div class="col-md-6">
+              <h4><p style="text-align: center;"> <span class="badge bg-primary"> First Year Courses : semester 1</span></p></h4>
 
               <table class="table table-bordered table-striped" id="CoursesTable" style="width:100%; font-family: 'Times New Roman'">
               <thead>
@@ -56,6 +56,39 @@ $this->params['breadcrumbs'] = [
               </thead>
             
               <?php foreach($courses1 as $key =>$courses): ?>
+                <?php 
+                if($courses->course_semester == 1):
+                ?>
+              
+                  <tbody>
+                  <?php $i=0; ?>
+                  <tr>
+                  <td><?= $key = $key + 1 ?></td>
+                  <td><?= $courses->course_code ?> </td>
+                  <td><?= $courses->course_name ?> </td>
+                  <td><?= $courses->course_credit ?> </td>
+                  <td><?= strtoupper( $courses->course_status ) ?> </td>
+                  </tr> 
+                  </tbody>
+                  <?php endif; ?>
+             
+              <?php endforeach ?>
+              </table>
+              </div>
+              <div class="col-md-6">
+              <h4><p style="text-align: center;"> <span class="badge bg-primary">First Year Courses : semester 2</span></p></h4>
+
+              <table class="table table-bordered table-striped" id="CoursesTable" style="width:100%; font-family: 'Times New Roman'">
+              <thead>
+              <tr>
+              <th width="1%">#</th><th width="10%">Code</th><th>Name</th><th>Credit</th><th>Status</th>
+              </tr>
+              </thead>
+            
+              <?php foreach($courses1 as $key =>$courses): ?>
+                <?php 
+                if($courses->course_semester == 2):
+                ?>
               
                   <tbody>
                   <?php $i=0; ?>
@@ -68,15 +101,15 @@ $this->params['breadcrumbs'] = [
                   </tr> 
                   </tbody>
                  
-             
+                <?php endif; ?>
               <?php endforeach ?>
               </table>
               </div>
              </div>
 
              <div class="row">
-             <div class="col-md-12">
-             <h4><p style="text-align: center;"> <span class="badge bg-warning">Second Year Courses</span></p></h4>
+             <div class="col-md-6">
+             <h4><p style="text-align: center;"> <span class="badge bg-warning">Second Year Courses : semester 1</span></p></h4>
               <table class="table table-bordered table-striped" id="CoursesTable" style="width:100%; font-family: 'Times New Roman'">
               <thead>
               <tr>
@@ -85,6 +118,9 @@ $this->params['breadcrumbs'] = [
               </thead>
             
               <?php foreach($courses2 as $key =>$courses): ?>
+                <?php 
+                if($courses->course_semester == 1):
+                ?>
               
                   <tbody>
                   <?php $i=0; ?>
@@ -97,15 +133,44 @@ $this->params['breadcrumbs'] = [
                   </tr> 
                   </tbody>
                  
-             
+                  <?php endif; ?>
+              <?php endforeach ?>
+              </table>
+              </div>
+              <div class="col-md-6">
+             <h4><p style="text-align: center;"> <span class="badge bg-warning">Second Year Courses : semester 2</span></p></h4>
+              <table class="table table-bordered table-striped" id="CoursesTable" style="width:100%; font-family: 'Times New Roman'">
+              <thead>
+              <tr>
+              <th width="1%">#</th><th width="10%">Code</th><th>Name</th><th>Credit</th><th>Status</th>
+              </tr>
+              </thead>
+            
+              <?php foreach($courses2 as $key =>$courses): ?>
+                <?php 
+                if($courses->course_semester == 2):
+                ?>
+              
+                  <tbody>
+                  <?php $i=0; ?>
+                  <tr>
+                  <td><?= $key = $key + 1 ?></td>
+                  <td><?= $courses->course_code ?> </td>
+                  <td><?= $courses->course_name ?> </td>
+                  <td><?= $courses->course_credit ?> </td>
+                  <td><?= strtoupper( $courses->course_status ) ?> </td>
+                  </tr> 
+                  </tbody>
+                 
+                  <?php endif; ?>
               <?php endforeach ?>
               </table>
               </div>
              </div>
 
              <div class="row">
-             <div class="col-md-12">
-             <h4><p style="text-align: center;"> <span class="badge bg-success">Third Year Courses</span></p></h4>
+             <div class="col-md-6">
+             <h4><p style="text-align: center;"> <span class="badge bg-success">Third Year Courses : semester 1</span></p></h4>
               <table class="table table-bordered table-striped" id="CoursesTable" style="width:100%; font-family: 'Times New Roman'">
               <thead>
               <tr>
@@ -114,6 +179,9 @@ $this->params['breadcrumbs'] = [
               </thead>
             
               <?php foreach($courses3 as $key =>$courses): ?>
+                <?php 
+                if($courses->course_semester == 1):
+                ?>
               
                   <tbody>
                   <?php $i=0; ?>
@@ -126,8 +194,37 @@ $this->params['breadcrumbs'] = [
                   </tr> 
                   </tbody>
                  
-             
-              <?php endforeach ?>
+                  <?php endif; ?>
+              <?php endforeach; ?>
+              </table>
+              </div>
+              <div class="col-md-6">
+             <h4><p style="text-align: center;"> <span class="badge bg-success">Third Year Courses : semester 2</span></p></h4>
+              <table class="table table-bordered table-striped" id="CoursesTable" style="width:100%; font-family: 'Times New Roman'">
+              <thead>
+              <tr>
+              <th width="1%">#</th><th width="10%">Code</th><th>Name</th><th>Credit</th><th>Status</th>
+              </tr>
+              </thead>
+            
+              <?php foreach($courses3 as $key =>$courses): ?>
+                <?php 
+                if($courses->course_semester == 2):
+                ?>
+              
+                  <tbody>
+                  <?php $i=0; ?>
+                  <tr>
+                  <td><?= $key = $key + 1 ?></td>
+                  <td><?= $courses->course_code ?> </td>
+                  <td><?= $courses->course_name ?> </td>
+                  <td><?= $courses->course_credit ?> </td>
+                  <td><?= strtoupper( $courses->course_status ) ?> </td>
+                  </tr> 
+                  </tbody>
+                 
+                  <?php endif; ?>
+              <?php endforeach; ?>
               </table>
               </div>
              </div>
