33c5c1b58db9eb24ab44d51d408adc8895cf0e32
khalideyez <onecampus2019@gmail.com>
2022-05-07 10:30:38 +0300

    student responsiveness for mobile 99%

--- frontend/views/student/submit_assignment.php
+++ frontend/views/student/submit_assignment.php
@@ -28,7 +28,7 @@ $this->params['breadcrumbs'] = [
    
         <div class="container">
                     <div class="card shadow-lg" style="font-family:'Times New Roman', sans-serif">
-                        <div class="card-header text-center bg-primary p-1">
+                        <div class="card-header text-center bg-primary p-1 pt-2">
                             <h5 class="responsivetext"><i class="fa fa-upload"></i> Submit Assignment </h5>
                         </div>
                         <div class="card-body">
